b1e320b09a742f0b16bebd0ff5501898a51f6682
taeung <youtanlai@gmail.com>
2016-12-12 16:08:16 +0700

    up1

--- mobifone/app/Http/Controllers/usercontroller.php
+++ mobifone/app/Http/Controllers/usercontroller.php
@@ -3,8 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\user;
+use Illuminate\Support\Facades\Auth;
 class usercontroller extends Controller
 {
-    //
+    public function getdanhsach(){
+        $user = user::all();
+        return view('admin.user.danhsach',['user'=>$user]);
+    }
+    function getthem(){
+    	return view('admin.user.them');
+    }
+    function postthem(Request $re){
+    	$this->validate($re,['ten'=>'required|unique:users,name|min:3|max:50','email'=>'required|unique:Users,email|email','pass'=>'required','repass'=>'required|same:pass'],
+    		['ten.required'=>'Tên không được bỏ trống','ten.unique'=>'Tên người dùng đã tồn tại','ten.min'=>'Tên phải nhiều hơn 3 kí tự và nhỏ hơn 50 kí tự','ten.max'=>'Tên phải nhiều hơn 3 kí tự và nhỏ hơn 50 kí tự',
+    		'email.required'=>'email không được bỏ trống','email.unique'=>'Email đã tồn tại ','email.email'=>'Không đúng định dạng email',
+    		'pass.required'=>'mật khẩu không được bỏ trống',
+    		'repass.required'=>'Nhập lại mật khẩu không được bỏ trống','repass.same'=>'Nhập lại mật khẩu không đúng']);
+    	$user = new user;
+    	$user->name = $re->ten;
+    	$user->email = $re->email;
+    	$user->password = bcrypt($re->pass);
+    	$user->quyen = $re->quyen;
+    	$user->save();
+    	return redirect('admin/user/them')->with('thongbao','Thêm thành công');
+    }
+    function getsua($id){
+    	$user = user::find($id);
+    	return view('admin.user.sua',['user'=>$user]);
+    }
+    function postsua(Request $re,$id){
+    		$this->validate($re,['ten'=>'required|unique:users,name|min:3|max:50','pass'=>'required','repass'=>'required|same:pass'],
+    		['ten.required'=>'Tên không được bỏ trống','ten.unique'=>'Tên người dùng đã tồn tại','ten.min'=>'Tên phải nhiều hơn 3 kí tự và nhỏ hơn 50 kí tự','ten.max'=>'Tên phải nhiều hơn 3 kí tự và nhỏ hơn 50 kí tự',
+    		'pass.required'=>'mật khẩu không được bỏ trống',
+    		'repass.required'=>'Nhập lại mật khẩu không được bỏ trống','repass.same'=>'Nhập lại mật khẩu không đúng']);
+    	$user = user::find($id);
+    	$user->name = $re->ten;
+    	$user->password = bcrypt($re->pass);
+    	$user->quyen = $re->quyen;
+    	$user->save();
+    	return redirect('admin/user/sua/'.$id)->with('thongbao','Sửa thành công');
+    }
+    function getxoa($id){
+    	$user = user::find($id);
+    	$user->delete();
+    	return redirect('admin/user/xoa/'.$id)->with('thongbao','Xóa thành công');
+    }
+    function getdangnhap(){
+    	return view('admin.dangnhap');
+    }
+    function postdangnhap(Request $re){
+
+          $this->validate($re,['email'=>'required','password'=>'required'],['email.required'=>'email không được bỏ trống','password.required'=>'password không được bỏ trống']);
+
+          if(Auth::Attempt(['email'=>$re->email,'password'=>$re->password])){
+          	    return redirect('admin/loaikh/danhsach');
+          }
+          else{
+          	    return redirect('admin/dangnhap')->with('thongbao','Đăng nhập thất bại');
+          }        	
+    }
+    function getdangxuat(){
+        Auth::logout();
+        return redirect('admin/dangnhap');
+    }
 }
---
+++ mobifone/resources/views/admin/user/danhsach.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layout.index')
+@section('content')
+<div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Người dùng
+                            <small>Danh sách</small>
+                        </h1>
+                    </div>
+                    @if(session('thongbao'))
+                    <div class="alert alert-success">
+                        {{session('thongbao')}}
+                    </div>
+                    @endif
+                    <!-- /.col-lg-12 -->
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr align="center">
+                                <th>ID</th>
+                                <th>Tên</th>
+                                <th>Email</th>                                
+                                <th>Quyền</th>
+                                <th>Xóa</th>
+                                <th>Sửa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($user as $us)
+                            <tr class="odd gradeX" align="center">
+                                <td>{{$us->id}}</td>
+                                <td>{{$us->name}}</td>
+                                <td>{{$us->email}}</td>
+                                <td>@if($us->quyen == 1)
+                                      {{"admin"}}
+                                    @else
+                                       {{"Thường"}}  
+                                    @endif   
+                                </td>
+                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="admin/user/xoa/{{$us->id}}"> Xóa</a></td>
+                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="admin/user/sua/{{$us->id}}">Sửa</a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+@endsection
